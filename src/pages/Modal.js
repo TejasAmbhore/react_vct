ccf619997172ef357e2742a9173d9ef7e75a8920
Tejas Ambhore <tejasam2020@gmail.com>
2022-03-04 02:29:26 +0530

    hover bug fixed, ins. updated

--- src/pages/Modal.js
+++ src/pages/Modal.js
@@ -43,11 +43,12 @@ function MyVerticallyCenteredModal(props) {
       >
         <div style={{ padding: "7%", color: "#ffffff", fontWeight: "500", fontSize: "20px", lineHeight: "2.4" }}>
           1.	Click on "Let's Go" button on the page that appears to start the game. Beware… the countdown begins! So be quick.<br />
-          2.	On the screen, a clue indicating a location on the IIT KGP campus emerges. Guess the location and click on the respective pointer on the map.<br />
-          3.	The pointer you choose opens an image of the same. This image contains spme vectors out of which only 1 vector is correct. Find that one, and you get the next hint on your screen.<br />
-          4.	Click the Key icon in the top-left corner of the screen to see the clue once more. This area of the screen also allows you to check your score.<br />
-          5.	To win interesting prizes, complete all of the riddles in the same way, as quickly as possible.<br />
-          6.	Make sure you complete the VCT in one sitting. OR the time keeps increasing <br />
+          2.  Do not use the back and forward buttons of your laptop. Use the one displayed on the screen.
+          3.	On the screen, a clue indicating a location on the IIT KGP campus emerges. Guess the location and click on the respective pointer on the map.<br />
+          4.	The pointer you choose opens an image of the same. This image contains spme vectors out of which only 1 vector is correct. Find that one, and you get the next hint on your screen.<br />
+          5.	Click the Key icon in the top-left corner of the screen to see the clue once more. This area of the screen also allows you to check your score.<br />
+          6.	To win interesting prizes, complete all of the riddles in the same way, as quickly as possible.<br />
+          7.	Make sure you complete the VCT in one sitting. OR the time keeps increasing <br />
           PS: The music starts as you click on the Start button and you are not allowed to stop that <RiGhost2Line /> Refreshing the page stops the music, to play it again, click on the sound icon in the top-left of the screen
           PPS: Click on F11 button for Better Experience<RiGhost2Line />
         </div>
